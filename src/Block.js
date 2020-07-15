88b7d31d55ed513244ec27a55eb73713f3039ff5
coder <aicode@protonmail.com>
2020-07-16 00:33:59 +0500

    Initialized

---
+++ src/Block.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Block(props) {
+    return (
+        <div className="App-header">
+            My {props.blockName} new component created.
+        </div>
+    )
+}
+
+export default Block;
