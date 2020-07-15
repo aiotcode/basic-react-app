109d4b7b429be06b35508155b49b8ac4764bd4c9
coder <aicode@protonmail.com>
2020-07-16 00:55:55 +0500

    Assignment added

--- src/Block.js
+++ src/Block.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Block(props) {
     return (
         <div className="App-header">
-            My {props.blockName} new component created.
+            My {props.blockName} # 3 new component created.
         </div>
     )
 }
